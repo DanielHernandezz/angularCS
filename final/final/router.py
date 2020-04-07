a12b92fe300f48bc8b0c74ab67d4a137823fb82a
MiceliLanda <183381@ids.upchiapas.edu.mx>
2020-04-07 12:00:41 -0500

    act

--- final/final/router.py
+++
@@ -1,5 +0,0 @@
-from Personas.viewsets import PersonasViewSet
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('register', PersonasViewSet)
---
+++ src/app/customer.ts
@@ -0,0 +1,7 @@
+export class Customer {
+    id:number;
+    nombre: string;
+    apPat: number;
+    apMat: boolean;
+    edad: number;
+}
